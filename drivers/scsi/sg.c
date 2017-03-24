aaddb4d24857aca06fcee196a7e5e071f8928189
Robb Glasser <rglasser@google.com>
2017-03-24 16:23:37 -0700

    Prevent potential double frees in sg driver
    sg_ioctl could be spammed by requests, leading to a double free in
    __free_pages. This protects the entry points of sg_ioctl where the
    memory could be corrupted by a double call to __free_pages if multiple
    requests are happening concurrently.
    
    Bug:35644812
    
    Change-Id: Ie13f65beb6974430f90292e2742841b26aecb8b1
    Signed-off-by: Robb Glasser <rglasser@google.com>

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -921,6 +921,7 @@ sg_ioctl(struct file *filp, unsigned int cmd_in, unsigned long arg)
 			sfp->low_dma = 1;
 			if ((0 == sfp->low_dma) && (0 == sg_res_in_use(sfp))) {
 				val = (int) sfp->reserve.bufflen;
+				mutex_lock(&sfp->parentdp->open_rel_lock);
 				sg_remove_scat(sfp, &sfp->reserve);
 				sg_build_reserve(sfp, val);
 				mutex_unlock(&sfp->parentdp->open_rel_lock);
@@ -998,6 +999,7 @@ sg_ioctl(struct file *filp, unsigned int cmd_in, unsigned long arg)
 		if (val != sfp->reserve.bufflen) {
 			if (sg_res_in_use(sfp) || sfp->mmap_called)
 				return -EBUSY;
+			mutex_lock(&sfp->parentdp->open_rel_lock);
 			sg_remove_scat(sfp, &sfp->reserve);
 			sg_build_reserve(sfp, val);
 			mutex_unlock(&sfp->parentdp->open_rel_lock);

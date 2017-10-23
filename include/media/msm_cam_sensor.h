d808673194b63be8831de7eefd8e66c316d1d1d4
Louis Popi <theh2o64@gmail.com>
2017-10-23 13:47:20 +0000

    land: Import Xiaomi Camera Changes

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -1,6 +1,3 @@
-#ifdef CONFIG_MACH_XIAOMI_LAND
-#include "msm_cam_sensor-land.h"
-#else
 #ifndef __LINUX_MSM_CAM_SENSOR_H
 #define __LINUX_MSM_CAM_SENSOR_H
 
@@ -281,4 +278,4 @@ struct msm_flash_cfg_data_t32 {
 #endif
 
 #endif
-#endif /* XIAOMI_LAND */
+
